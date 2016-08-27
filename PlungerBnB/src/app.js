a618e9f5117af5571d6cb878f5fe6bb443ab665e
Harry Wang <harrywang@ENG-LAP-60.local>
2016-08-27 17:31:52 -0500

    send unique device token, and accept_job end point to api

--- PlungerBnB/src/app.js
+++ PlungerBnB/src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage, AlertIOS } from 'react-native';
 
 import styles from './styles/styles'
 import Header from './components/header'
@@ -12,8 +12,40 @@ class UpdateLocation extends Component {
     this.state = {
       showText: true,
       latitude: "",
-      longitude: ""
+      longitude: "",
+      uniqueDeviceToken: "",
+      requestingPlunger: false,
+      currentlyDelivering: false,
+      alertIsShown: false,
+      requestorId: null,
+      deliverCoords: {latitude: '', longitude: ''}
     };
+
+    var uniqueDeviceToken;
+    AsyncStorage.getItem('uniqueDeviceToken').then((value) => {
+      if (value) {
+        console.log("value exists!! this is value");
+        console.log(value);
+        uniqueDeviceToken = value;
+      } else {
+        console.log("value NOOOT exists!!");
+        uniqueDeviceToken = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        AsyncStorage.setItem('uniqueDeviceToken', uniqueDeviceToken);
+        console.log("this is uniqueDeviceToken");
+        console.log(uniqueDeviceToken);
+      }
+      this.setState({uniqueDeviceToken: uniqueDeviceToken});
+    }).done();
+
+    // var uniqueDeviceToken = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    //
+    // AsyncStorage.setItem('uniqueDeviceToken', uniqueDeviceToken);
+    //
+    // this.setState({'uniqueDeviceToken': uniqueDeviceToken});
+
+    console.log(uniqueDeviceToken);
+    console.log("this is this.state");
+    console.log(this.state);
 
     // Toggle the state every second
     setInterval(() => {
@@ -32,9 +64,49 @@ class UpdateLocation extends Component {
             },
             body: JSON.stringify({
               latitude: userLatitude,
-              longitude: userLongitude
+              longitude: userLongitude,
+              unique_device_token: this.state.uniqueDeviceToken,
+              requesting_plunger: this.state.requestingPlunger
             })
-          });
+          })
+          .then((response) => response.json())
+          .then((responseData) => {
+            console.log(responseData);
+            if (!this.state.currentlyDelivering && !this.state.alertIsShown) {
+              AlertIOS.alert(
+               'Someone requested a plunger!!!!!',
+               'Would you like to deliver??',
+               [
+                 {text: 'No', onPress: () => this.setState({alertIsShown: false}), style: 'cancel'},
+                 {text: 'Yes', onPress: () => {
+                   this.setState({requestorId: responseData.id});
+                   console.log("this is repsonseData.id");
+                   console.log(responseData.id);
+                   fetch('http://172.28.116.238:3000/accept_job', {
+                     method: 'POST',
+                     headers: {
+                       'Accept': 'application/json',
+                       'Content-Type': 'application/json',
+                     },
+                     body: JSON.stringify({
+                       latitude: userLatitude,
+                       longitude: userLongitude,
+                       unique_device_token: this.state.uniqueDeviceToken,
+                       requestor_id: this.state.requestorId
+                     })
+                   });
+                   this.setState({
+                     alertIsShown: false,
+                     currentlyDelivering: true,
+                     deliverCoords: {latitude: responseData.latitude, longitude: responseData.longitude}}
+                   );
+                 }},
+               ],
+              );
+
+              this.setState({alertIsShown: true});
+            }
+          }).done();
         },
         (error) => alert(error.message),
         {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
--- PlungerBnB/src/components/header.js
+++ PlungerBnB/src/components/header.js
@@ -8,7 +8,20 @@ class Header extends Component {
 
   _handleRequestClick() {
     console.log("Request Plunger!");
-    fetch("http://172.28.116.238:3000/create_user");
+    fetch('http://172.28.116.238:3000/request_plunger', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        latitude: userLatitude,
+        longitude: userLongitude,
+        unique_device_token: this.state.uniqueDeviceToken
+      })
+    })
+    .then((response) => response.json())
+    .then((responseData) => console.log(responseData)).done();
   }
 
   _handleOfferClick() {
